11625857347df4d95c321b69b82096f2dd7fe4ea
JesusMatusA <jesus.matus.2000@gmail.com>
2022-05-12 15:29:00 -0700

    Matus Codigo para trabajar

---
+++ Screens/recepcionistScreen.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../Styles/recepcionistScreenStyles.css">
+    <title>Recepcionista</title>
+</head>
+<body>
+    <nav class="navigatorBarContainer">
+        <div class="logoContainer">
+            <img src="./img/logo.jpg" alt="" class="imgLogo">
+        </div>
+        <div class="nameContainer">Manos de Angel Clínica y Spa</div>
+    </nav>
+    <div class="bodyContainer">
+        <div class="optionsContainer">
+            <div class="menuContainer">
+                <div class="imgRecepcionistContainer">
+                    <img src="/img/recepcionist.png" alt="" class="recepcionistLogo">
+                </div>
+                <div class="buttonsContainer">
+                    <input type="button" value="Agendar Cita" class="btnOption">
+                    <input type="button" value="Consultar Cita" class="btnOption">
+                    <input type="button" value="Consultar Cliente" class="btnOption">
+                </div>
+                <div class="logoutContainer">
+                    <input type="button" value="Logout" class="btnLogout">
+                </div>
+            </div>
+        </div>
+        <div class="showsContainer">Opciones Mostradas</div>
+    </div>
+    <footer class="footerContainer">sasd</footer>
+</body>
+</html>
